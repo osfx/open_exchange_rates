6deb4578692fe897b56e94e2c7e6db6cdf3cacb4
osfx <osfx42@gmail.com>
2016-07-27 11:19:34 +0300

    Fix docs

--- src/open_exchange_rates/mapping.cr
+++ src/open_exchange_rates/mapping.cr
@@ -1,14 +1,14 @@
-require "json"
-
-module OpenExchangeRates
-  module Mapping
-    # abstract class Base
-    #   def initialize(@output, @response)
-    #   end
-    #
-    #   def mapper_name
-    #     self.class
-    #   end
-    # end
-  end
-end
+# require "json"
+#
+# module OpenExchangeRates
+#   module Mapping
+#     # abstract class Base
+#     #   def initialize(@output, @response)
+#     #   end
+#     #
+#     #   def mapper_name
+#     #     self.class
+#     #   end
+#     # end
+#   end
+# end
